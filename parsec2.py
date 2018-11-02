327812150b93c70ca92ad55659d33988944ae22a
Bryan <bryches@gmail.com>
2018-11-02 13:00:46 -0400

    Updates

--- parsec2.py
+++ parsec2.py
@@ -1,5 +1,7 @@
 import requests
 import json
+from datetime import datetime
+
 
 
 
@@ -9,7 +11,33 @@ def get_index(year, qtr):
     base_url = "https://www.sec.gov/Archives/edgar/full-index/"
     full_url = base_url + str(year) + "/QTR" + str(qtr) + "/form.idx"
 
-    data  = requests.get(full_url).text
+    return requests.get(full_url).text
+
+
+
+def save_index(year, qtr, index):
+    filename = 'index-' + str(year) + 'q' + str(qtr) + '.json'
+
+    with open('index/' + filename, 'w') as file:
+        json.dump(index, file)
+
+
+
+def load_index(year, qtr):
+    filename = 'index-' + str(year) + 'q' + str(qtr) + '.json'
+
+    try:
+        with open('index/' + filename, 'r') as file:
+            return json.load(file)
+
+    except FileNotFoundError:
+        index = parse_index(get_index(year, qtr))
+        save_index(year, qtr, index)
+        return index
+
+
+
+def parse_index(data):
     lines = data.splitlines()
     index = {}
 
@@ -31,9 +59,37 @@ def get_index(year, qtr):
 
 
 
-
 def get_report(filename):
     return requests.get('https://www.sec.gov/Archives/' + filename).text
+
+
+
+
+
+def save_report(info, report):
+    name = info['name']
+    
+    for char in ['.', '/', '-', ',', '&', '(', ')']:
+        name = name.replace(char, '')
+
+    filename = name.lower().replace(' ', '-') + '-' + info['date'] + '.html'
+
+    a = report.find('<html>')
+    b = report.find('<HTML>')
+
+    if   a > -1 and b > -1: i = min(a, b)
+    elif a > -1 or  b > -1: i = a if a > -1 else b
+    else: i = False
+
+    opening_tag = report[i:i + 6] if i else False
+    closing_tag = '</html>' if opening_tag == '<html>' else '</HTML>'
+
+    j = report.find(closing_tag, i) if i else False
+
+    if i and j:
+        with open('reports/html/' + filename, 'w') as file:
+            file.write(report[i:j + 7])
+
 
 
 
@@ -107,7 +163,7 @@ def parse_value(snip, year):
     value = False
     tries = 0
 
-    while not value and tries < 100:
+    while not value and tries < 50:
         i = snip.find('>')
         if i > -1:
             snip = snip[i + 1:]
@@ -150,7 +206,7 @@ def parse_metric(page, triggers, year):
         snip  = True
         value = False
 
-        while snip and not value:
+        while snip and not value and start < 500000:
             snip, start = get_snippet(page, trigger, start)
 
             if snip:
@@ -164,21 +220,73 @@ def parse_metric(page, triggers, year):
 
 
 
+def report_is_valid(results):
+    r = results
+
+    if (r['revenue']
+        and r['net_income']
+        and r['current_assets']
+        and r['total_assets']
+        and r['current_liabilities']
+    ):
+        if (r['total_liabilities']
+            or (r['total_equity'] and r['total_liabilities_and_equity'])
+        ):
+            return True
+
+    return False
+
+
+
+
+
+
+def calc_ratios(data):
+    equity = data['total_equity'] if data['total_equity'] else data['total_assets'] - data['total_liabilities']
+
+    total_liabilities = data['total_liabilities'] if data['total_liabilities'] else data['total_liabilities_and_equity'] - equity
+
+    output = {
+        'profit_margin':    float(data['net_income'])     / data['revenue'],
+        'return_on_equity': float(data['net_income'])     / equity,
+        'debt_coverage':    float(data['net_income'])     / data['current_liabilities'],
+        'current_leverage': float(data['current_assets']) / data['current_liabilities'],
+        'total_leverage':   float(data['total_assets'])   / total_liabilities
+    }
+
+    for metric in output:
+        output[metric] = round(output[metric], 5)
+
+    return output
+
+
+
+
+
+
+
+
+
+
+yr = 2018
 triggers = load_triggers()
-index    = get_index(2018, 2)
+index = load_index(yr, 2)
+
 
 for cik in index:
-    file   = index[cik]['file']
-    report = get_report(file)
+    if 'metrics' not in index[cik]:
+        report = get_report(index[cik]['file'])
+        name   = index[cik]['name']
+        date   = index[cik]['date']
 
-    print('-' * 35)
-    print('Name'.ljust(22) + ': ' + index[cik]['name'])
-    print('CIK'.ljust(22)  + ': ' + cik)
-    print('Date'.ljust(22) + ': ' + index[cik]['date'])
-    print('')
+        results = {}
+        for metric in triggers:
+            results[metric] = parse_metric(report, triggers[metric], yr)
 
-    for metric in triggers:
-        result = parse_metric(report, triggers[metric], 2018)
-        if not result:
-            print(metric.ljust(22) + ': ' + str(result))
-    print('-' * 35 + '\n\n')
+        if report_is_valid(results):
+            index[cik]['metrics'] = results
+            index[cik]['ratios']  = calc_ratios(results)
+        else:
+            index[cik]['metrics'] = False
+
+save_index(yr, 2, index)
